e74ef6372b407ff5763b7df01ebaefbaf77ede9b
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2020-10-27 16:00:29 -0500

    initial commit after spinning up react app

---
+++ genie/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <div id="login">
+        <h1>First, log in to spotify</h1>
+        <a href="/login">Log in</a>
+        </div>
+        <div id="loggedin">
+        </div>
+    </div>
+
+  );
+}
+
+export default App;
